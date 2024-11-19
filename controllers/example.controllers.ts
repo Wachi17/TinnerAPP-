9af0ed30cce4061a963b155c53b1412df6d030bd
Wachi17 <wachirawit.ho@rmuti.ac.th>
2024-11-19 23:15:43 +0700

    init

---
+++ controllers/example.controllers.ts
@@ -0,0 +1,25 @@
+import { Elysia, t } from "elysia"
+export const example = new Elysia()
+    .get("/", () => "Hello World",{
+        detail:{
+            tags:["Example"],
+            summary:"Get Hello World",
+            description:"bla bla bla"
+        }
+    })
+    .post("/about/", ({ body }) => {
+        return {
+            id: "123",
+            msg: "hello" + body.name
+        }
+
+    }, {
+        body: t.Object({
+            name: t.String()
+        }),
+        detail:{
+            tags:["Example"],
+            summary:"About",
+            description:"bla bla bla"
+        }
+    })
